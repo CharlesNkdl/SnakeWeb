2814e364a12e6e00416ed92dc5800295d8a9d738
charles <charles.nikdel@gmail.com>
2023-06-02 02:17:06 +0200

    Ajout Leaderboard et base de donne firestore pour la gestion

--- public/snake.js
+++ public/snake.js
@@ -1,3 +1,18 @@
+// Initialise Firebase
+var firebaseConfig = {
+	apiKey: "AIzaSyCYFoqW_hAoe4EPaRe1I1rpnDmkN2z4Nyc",
+	authDomain: "snake42bg.firebaseapp.com",
+	projectId: "snake42bg",
+	storageBucket: "snake42bg.appspot.com",
+	messagingSenderId: "85272090541",
+	appId: "1:85272090541:web:0dc3958a4633aae28bf324"
+	};
+
+  // Initialise l'application Firebase
+  var app = firebase.initializeApp(firebaseConfig);
+
+  // Référence à la base de données
+  var db = firebase.firestore(app);
 class Snake {
 	constructor(x, y, size) {
 			this.x = x;
@@ -37,8 +52,6 @@ class Snake {
 
 class Apple {
 	constructor() {
-			console.log("apple")
-			console.log(snake.size)
 			var isTouching;
 			while(true){
 				isTouching = false;
@@ -49,7 +62,6 @@ class Apple {
                         isTouching = true;
                     }
                 }
-				console.log(this.x, this.y)
 				this.color = "pink";
 				this.size = snake.size;
 				if(!isTouching){
@@ -58,14 +70,30 @@ class Apple {
 			}
 		}
 }
+// ...
 
-
-
-
-
+// Fonction pour sauvegarder le score avec le nom du joueur
+function saveScore(score) {
+	var playerName = prompt("Entrez votre nom :");
+	if (playerName) {
+	  var leaderboardRef = db.collection("leaderboard");
+	  var newScore = {
+		player: playerName,
+		score: score,
+		timestamp: firebase.firestore.Timestamp.now()
+	  };
+	  leaderboardRef.add(newScore)
+		.then(function(docRef) {
+		  console.log("Score enregistré avec l'ID :", docRef.id);
+		})
+		.catch(function(error) {
+		  console.error("Erreur lors de l'enregistrement du score :", error);
+		});
+	}
+  }
 var canvas = document.getElementById('canvas')
 
-var snake = new Snake(20, 20, 20);
+var snake = new Snake(40, 40, 40);
 
 var apple = new Apple();
 
@@ -73,12 +101,14 @@ var isGameOver = false;
 
 var canvasContext = canvas.getContext('2d');
 
+
 window.onload = () => {
 	gameLoop();
+	updateLeaderboard();
 }
 
 function gameLoop() {
-	setInterval(show, 1000/17)
+	setInterval(show, 1000/20)
 }
 
 function show() {
@@ -109,7 +139,7 @@ function checkGameOver() {
         if (head.x === snake.tail[i].x && head.y === snake.tail[i].y) {
 			if (!(head.x === apple.x && head.y === apple.y)) {
 				isGameOver = true;
-            	break; // Le jeu continue si la tête touche la pomme
+            	break;
 			}
         }
     }
@@ -120,20 +150,20 @@ function gameOver() {
 		snake.rotateY = 0;
 
 		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-
 		canvasContext.font = "40px Arial";
 		canvasContext.fillStyle = "red";
 		canvasContext.textAlign = "center";
 		canvasContext.fillText("Game Over", canvas.width / 2, canvas.height / 2 - 20);
 		canvasContext.fillText("Score: " + (snake.tail.length - 1), canvas.width / 2, canvas.height / 2 + 20);
 		canvasContext.fillText("Press `R` to retry !", canvas.width / 2, canvas.height / 2 + 60);
-
+		saveScore(snake.tail.length - 1);
 	}
 
 function resetGame() {
-	snake = new Snake(20, 20, 20);
+	snake = new Snake(40, 40, 40);
 	apple = new Apple();
 	isGameOver = false;
+	updateLeaderboard();
 }
 
 function checkHitWall() {
@@ -200,6 +230,48 @@ window.addEventListener("keydown", (event) => {
         }
     }
 });
+
+function getPlayerName() {
+	var playerNameInput = document.getElementById("playerNameInput");
+	console.log(playerNameInput.value);
+	return playerNameInput.value;
+  }
+
+  function updateLeaderboard() {
+	var leaderboardTable = document.getElementById("leaderboard-table");
+	leaderboardTable.innerHTML = "<tr><th>Date</th><th>Joueur</th><th>Score</th></tr>";
+
+	var leaderboardRef = db.collection("leaderboard").orderBy("score", "desc").limit(10);
+	leaderboardRef.get()
+	  .then(function(querySnapshot) {
+		querySnapshot.forEach(function(doc) {
+		  var scoreData = doc.data();
+		  var playerName = scoreData.player;
+		  var score = scoreData.score;
+		  var timestamp = scoreData.timestamp;
+		  var datefd = timestamp.toDate();
+		  var date = datefd.toLocaleDateString();
+
+		  var row = document.createElement("tr");
+		  var playerNameCell = document.createElement("td");
+		  var scoreCell = document.createElement("td");
+		  var dateCell = document.createElement("td");
+
+		  playerNameCell.textContent = playerName;
+		  scoreCell.textContent = score;
+		  dateCell.textContent = date;
+
+		  row.appendChild(dateCell);
+		  row.appendChild(playerNameCell);
+		  row.appendChild(scoreCell);
+		  leaderboardTable.appendChild(row);
+		});
+	  })
+	  .catch(function(error) {
+		console.error("Erreur lors de la récupération des scores :", error);
+	  });
+  }
+
 
 
 
